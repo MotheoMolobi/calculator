578619f48f3ade0e0080b46559206859f0215384
MotheoMolobi <molobimotheo@gmail.com>
2024-12-15 12:05:07 +0200

    first function (operate)

---
+++ script.js
@@ -0,0 +1,9 @@
+const eqlBtn = document.querySelector("#eql-btn") 
+
+eqlBtn.addEventListener("click", operate)
+
+//This function is called when the equal button is clicked
+//It will operate whatever is typed in the calculator
+function operate(){
+
+}
